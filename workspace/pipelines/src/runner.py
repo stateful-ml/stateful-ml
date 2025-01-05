6530579c65753aeb2c67440e06f25bcb62b83496
Iliya Malecki <iliyamalecki@gmail.com>
2025-01-05 21:30:42 +0100

    make runner 3.10 compatible

--- workspace/pipelines/src/runner.py
+++ workspace/pipelines/src/runner.py
@@ -1,13 +1,16 @@
-from typing import Callable, Iterator
+from typing import Callable, Iterator, TypeVar
+
+ET = TypeVar("ET")
+TL = TypeVar("TL")
 
 
-def run_etl[ET, TL](
+def run_etl(
     extract: Callable[[], Iterator[ET]],
     transform: Callable[[ET], TL],
     load: Callable[[TL], None],
 ):
-    '''
+    """
     the default runner is just a simple synchronous batched runner
-    '''
+    """
     for batch in extract():
         load(transform(batch))
